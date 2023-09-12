218c9078f6780c9fa1e1adf1c4ba414cb525b1c4
rohishamamam <rohishamamam@gmail.com>
2023-09-13 00:34:29 +0500

    Bug Fixed 2

--- utils/jsonWebTokenSend.tsx
+++ utils/jsonWebTokenSend.tsx
@@ -1,7 +1,7 @@
 import JWT from "jsonwebtoken"
 
  const JWTToken = (payload: {id: string}) => {
-    return JWT.sign(payload, process.env.SECRET_KEY);
+    return JWT.sign(payload, process.env.SECRET_KEY!);
 }
 
 export default JWTToken
