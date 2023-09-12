77e2ca9eca9f2913a65bd77a532d2a4f003ae765
rohishamamam <rohishamamam@gmail.com>
2023-09-13 00:31:32 +0500

    Bug Fixed

--- pages/api/checkAuth.tsx
+++ pages/api/checkAuth.tsx
@@ -1,16 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 import prisma from "@/utils/db";
+type Get = {
+  id: string;
+};
 export const checkAuth = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!req.headers.cookie) {
     return res.status(400).json({ success: false, msg: "Access Denied" });
   }
-
   const cook = req.headers.cookie?.split("=")[1];
-  const useId = jwt.verify(cook, process.env.SECRET_KEY);
-
+  const useId:any = jwt.verify(cook, process.env.SECRET_KEY!);
   const user = await prisma.user.findFirst({
     where: { id: useId.id },
   });
-  return user
+  return user;
 };
