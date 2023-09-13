1226f1c48d0a1c1989ebd78936aa1ca3447ddd1d
rohishamamam <rohishamamam@gmail.com>
2023-09-13 20:23:27 +0500

    major changea2a2

--- pages/api/auth/login.tsx
+++ pages/api/auth/login.tsx
@@ -33,7 +33,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     res.status(200).json({
         success: true,
         message: "Login Successfully",
-        token
+        // token
     })
   } catch (error:any) {
     return res.status(500).json({ success: false, message: error.message });
