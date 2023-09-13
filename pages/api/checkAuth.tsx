afe00ab1b7a4ffe92367dccbf2ff70de10328fda
rohishamamam <rohishamamam@gmail.com>
2023-09-14 01:38:29 +0500

    29

--- pages/api/checkAuth.tsx
+++ pages/api/checkAuth.tsx
@@ -6,7 +6,7 @@ export const checkAuth = async (req: NextApiRequest, res: NextApiResponse) => {
   if (!req.headers.cookie) {
     return res.status(400).json({ success: false, msg: "Access Denied" });
   }
-  const cook = req.headers.cookie?.split("=")[1];
+  const cook = JSON.parse(req.headers.cookie?.split("=")[1]);
   const useId:any = jwt.verify(cook, process.env.SECRET_KEY!);
   const user = await prisma.user.findFirst({
     where: { id: useId.id },
